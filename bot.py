b35563a948c4456ea284fc33e928027b52a2014a
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2020-06-26 08:36:01 -0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import time
+import Tkinter as tk
+
+from ant.core import driver
+from ant.core import node
+
+from PowerMeterTx import PowerMeterTx
+from config import DEBUG, LOG, NETKEY, POWER_SENSOR_ID
+
+antnode = None
+power_meter = None
+
+try:
+    stick = driver.USB2Driver(None, log=LOG, debug=DEBUG)
+    antnode = node.Node(stick)
+    print "Starting ANT node"
+    antnode.start()
+    key = node.NetworkKey('N:ANT+', NETKEY)
+    antnode.setNetworkKey(0, key)
+
+    print "Starting power meter with ANT+ ID " + repr(POWER_SENSOR_ID)
+    try:
+        # Create the power meter object and open it
+        power_meter = PowerMeterTx(antnode, POWER_SENSOR_ID)
+        power_meter.open()
+    except Exception as e:
+        print "power_meter error: " + e.message
+        power_meter = None
+
+    master = tk.Tk()
+    master.title("Bot")
+    master.geometry("200x50")
+    master.call('wm', 'attributes', '.', '-topmost', '1')
+    w = tk.Scale(master, from_=0, to=1000, length=200, orient=tk.HORIZONTAL)
+    w.pack()
+
+    t = 0
+    last = 0
+    power = 0
+
+    print "Main wait loop"
+    while True:
+        power = w.get()
+        t = int(time.time())
+        if power and t >= last + 1:
+            power_meter.update(power)
+        last = t
+        master.update_idletasks()
+        master.update()
+
+except:
+    pass
+finally:
+    if power_meter:
+        print "Closing power meter"
+        power_meter.close()
+        power_meter.unassign()
+    if antnode:
+        print "Stopping ANT node"
+        antnode.stop()
