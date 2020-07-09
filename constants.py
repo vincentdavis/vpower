f65d8536fd473adbde64cf6a6aff2afff8a31abc
oldnapalm <38410858+oldnapalm@users.noreply.github.com>
2020-07-09 16:41:18 -0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,19 +1,17 @@
+import subprocess
+
 SPEED_DEVICE_TYPE = 0x7B
 CADENCE_DEVICE_TYPE = 0x7A
 SPEED_CADENCE_DEVICE_TYPE = 0x79
 POWER_DEVICE_TYPE = 0x0B
 
 
-# Get the serial number of Raspberry Pi
+# Get the serial number of CPU
 def getserial():
-    # Extract serial from cpuinfo file
+    # Extract serial from wmic command
     cpuserial = "0000000000000000"
     try:
-        f = open('/proc/cpuinfo', 'r')
-        for line in f:
-            if line[0:6] == 'Serial':
-                cpuserial = line[10:26]
-        f.close()
+        cpuserial = subprocess.check_output('wmic cpu get ProcessorId').split('\n')[1].strip()
     except:
         cpuserial = "ERROR000000000"
 
